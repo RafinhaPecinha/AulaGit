7c1b9602d254d46757fe7628d8042e1573bb0f25
RafinhaPecinha <rafaplays.018@gmail.com>
2025-02-25 15:09:16 -0300

    teste

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-console.log("Olá, GIT!!!");
-=======
 console.log("Olá, GIT!!!")
 console.log("Primeiro Branch, Goti demais!!!")
 console.log("Segunda alteração no Branch Gotão")
->>>>>>> modulo1
-=======
-console.log("Olá, GIT!!!")
-console.log("Primeiro Branch, Goti demais!!!")
-console.log("Segunda alteração no Branch Gotão")
-console.log("Merge")
->>>>>>> cc3cb89565fc898e66633e4717d7a507972a84f8
